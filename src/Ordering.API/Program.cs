924e6384a301c9fb0b3ca81a08a659a02a6b047f
JakeRosa <joaquimvr15@gmail.com>
2025-03-10 19:01:26 +0000

    starting custom metrics

--- src/Ordering.API/Program.cs
+++ src/Ordering.API/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System.Diagnostics.Metrics;
+using OpenTelemetry.Metrics;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 builder.AddApplicationServices();
@@ -8,7 +11,29 @@ var withApiVersioning = builder.Services.AddApiVersioning();
 
 builder.AddDefaultOpenApi(withApiVersioning);
 
+// Creating meter
+var meter = new Meter("Ordering.API");
+builder.Services.AddSingleton(meter);
+builder.Services.AddSingleton(meter.CreateCounter<long>("order_placed_count", description: "Número total de orders."));
+
+// Config meters
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(metrics =>
+    {
+        metrics
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddMeter("Ordering.API")
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri("http://localhost:4317");
+            });
+    });
+
 var app = builder.Build();
+
+// I don't need this I think
+// app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.MapDefaultEndpoints();
 
